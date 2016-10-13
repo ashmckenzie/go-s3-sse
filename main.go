bf3d164d53806bb5819242072ac02f4bf4848bf4
Ash McKenzie <ash@the-rebellion.net>
2016-10-13 19:10:56 +1100

    OMG lots

--- main.go
+++ main.go
@@ -2,16 +2,20 @@ package main
 
 import (
   "fmt"
+  "log"
   "os"
   "runtime"
   "strconv"
+  "sync"
   "time"
 
-  "github.com/Sirupsen/logrus"
-  _ "github.com/mattn/go-sqlite3"
-  pb "gopkg.in/cheggaaa/pb.v1"
+  "net/http"
+  _ "net/http/pprof"
 
   "github.com/Bowbaq/profilecreds"
+
+  "github.com/Sirupsen/logrus"
+
   "github.com/aws/aws-sdk-go/aws"
   "github.com/aws/aws-sdk-go/aws/session"
   "github.com/aws/aws-sdk-go/service/s3"
@@ -35,116 +39,91 @@ var DiskLogger *logrus.Logger
 var s3Client *s3.S3
 var workerCount int
 
+var wg sync.WaitGroup
+
+const neededEncryptionType string = "AES256"
+
 type s3Object struct {
   Bucket     string
   Key        string
   Encryption string
 }
 
-func getS3Client(awsRegionName string, credentialsFileName string, roleName string) *s3.S3 {
-  profilecreds.DefaultDuration = 1 * time.Hour
-
-  creds := profilecreds.NewCredentials("virginia-admin", func(p *profilecreds.AssumeRoleProfileProvider) {
-    p.Cache = profilecreds.NewFileCache("")
-  })
-
+func setupS3Client(awsRegionName string, credentialsFileName string, roleName string) {
   config := aws.Config{
-    Credentials:      creds,
     S3ForcePathStyle: aws.Bool(false),
     Region:           aws.String(awsRegionName),
   }
 
-  return s3.New(session.New(), &config)
+  if len(roleName) > 0 {
+    profilecreds.DefaultDuration = 1 * time.Hour
+    creds := profilecreds.NewCredentials(roleName, func(p *profilecreds.AssumeRoleProfileProvider) {
+      p.Cache = profilecreds.NewFileCache("")
+    })
+    config.Credentials = creds
+  }
+
+  s3Client = s3.New(session.New(), &config)
 }
 
-func objectsFor(bucketName string) []*s3Object {
-  continuationToken := ""
-  startAfter := ""
-
-  var objects []*s3Object
-
-  // db, err := sql.Open("sqlite3", "./foo.db")
-  // if err != nil {
-  //   log.Fatal(err)
-  // }
-  // defer db.Close()
-  //
-  // sqlStmt := `
-  // create table objects (id integer not null primary key, bucket text, key text, encryption text);
-  // `
-  // _, err = db.Exec(sqlStmt)
-  // if err != nil {
-  //   log.Printf("%q: %s\n", err, sqlStmt)
-  // }
-  // INSERT OR REPLACE INTO Employee ("id", "name", "role") VALUES (1, "John Foo", "CEO")
-
-  Logger.Info("Getting list of objects..")
-
-  objects = examineObjects(objects, bucketName, continuationToken, startAfter)
-  objectCount := len(objects)
-
-  if objectCount == 0 {
-    Logger.Info("No objects!")
-    return objects
-  }
-
-  Logger.Info("Getting metadata for " + strconv.Itoa(objectCount) + " objects..")
-
-  jobs := make(chan *s3Object, objectCount)
-  results := make(chan bool, objectCount)
-
-  Logger.Info("Running with " + strconv.Itoa(workerCount) + " workers..")
-
-  bar := pb.StartNew(objectCount)
-  bar.Output = os.Stderr
-
-  for i := 1; i <= workerCount; i++ {
-    go getMetadataWorker(jobs, results)
-  }
-
-  for i := 0; i < objectCount; i++ {
-    jobs <- objects[i]
-  }
-  close(jobs)
-
-  for i := 0; i < objectCount; i++ {
-    <-results
-    bar.Increment()
-  }
-  close(results)
-
-  bar.Finish()
-
-  return objects
-}
-
-func getMetadataWorker(jobs <-chan *s3Object, results chan<- bool) {
+func getMetadataWorker(num int, jobs <-chan *s3Object, results chan<- *s3Object) {
   for object := range jobs {
-    params := &s3.HeadObjectInput{
-      Bucket: aws.String(object.Bucket),
-      Key:    aws.String(object.Key),
-    }
+    DiskLogger.Debug(fmt.Sprintf("getMetadataWorker(): num=[%d], object=[%v] - calling getMetaDataForObject()", num, object))
+    wg.Add(1)
+    go getMetaDataForObject(object, results)
+  }
+}
 
-    head, err := s3Client.HeadObject(params)
-    if err != nil {
-      Logger.Error("objectsFor(): s3Client.HeadObject(params) " + err.Error())
+func encryptObjectWorker(num int, jobs <-chan *s3Object, results chan<- *s3Object) {
+  for object := range jobs {
+    DiskLogger.Debug(fmt.Sprintf("encryptObjectWorker(): num=[%d], object=[%v] - calling encryptObject()", num, object))
+    wg.Add(1)
+    go encryptObject(object, results)
+  }
+}
+
+func encryptObject(object *s3Object, results chan<- *s3Object) {
+  copySource := fmt.Sprintf("%s/%s", object.Bucket, object.Key)
+
+  params := &s3.CopyObjectInput{
+    Bucket:               aws.String(object.Bucket),
+    Key:                  aws.String(object.Key),
+    CopySource:           aws.String(copySource),
+    ServerSideEncryption: aws.String(neededEncryptionType),
+  }
+
+  _, err := s3Client.CopyObject(params)
+
+  if err != nil {
+    Logger.Error("encryptObjectWorker(): s3Client.CopyObject(params) " + err.Error())
+  } else {
+    object.Encryption = neededEncryptionType
+  }
+
+  results <- object
+}
+
+func getMetaDataForObject(object *s3Object, results chan<- *s3Object) {
+  params := &s3.HeadObjectInput{
+    Bucket: aws.String(object.Bucket),
+    Key:    aws.String(object.Key),
+  }
+
+  head, err := s3Client.HeadObject(params)
+  if err != nil {
+    Logger.Error("getMetaDataForObject(): s3Client.HeadObject(params) " + err.Error())
+  } else {
+    if head.ServerSideEncryption != nil {
+      object.Encryption = *head.ServerSideEncryption
     } else {
-      if head.ServerSideEncryption != nil {
-        object.Encryption = *head.ServerSideEncryption
-      }
+      object.Encryption = "UNKNOWN"
     }
-
-    results <- true
   }
+
+  results <- object
 }
 
-func examineObjectWorker(bucketName string, jobs <-chan *s3.Object, results chan<- *s3Object) {
-  for object := range jobs {
-    results <- &s3Object{Bucket: bucketName, Key: *object.Key, Encryption: "????"}
-  }
-}
-
-func examineObjects(objects []*s3Object, bucketName string, continuationToken string, startAfter string) []*s3Object {
+func getObjectsForBucket(jobs chan<- *s3Object, results chan<- *s3Object, bucketName string, continuationToken string, startAfter string) {
   listObjectsParams := &s3.ListObjectsV2Input{
     Bucket: aws.String(bucketName),
   }
@@ -157,80 +136,120 @@ func examineObjects(objects []*s3Object, bucketName string, continuationToken st
   resp, err := s3Client.ListObjectsV2(listObjectsParams)
 
   if err != nil {
-    Logger.Error("objectsFor(): s3Client.ListObjectsV2(listObjectsParams) " + err.Error())
+    Logger.Error("getObjectsForBucket(): s3Client.ListObjectsV2(listObjectsParams) " + err.Error())
   }
 
   jobCount := 0
-  jobs := make(chan *s3.Object, 1000)
-  results := make(chan *s3Object, 1000)
-
-  for i := 1; i <= workerCount; i++ {
-    go examineObjectWorker(bucketName, jobs, results)
-  }
 
   for _, tmpObject := range resp.Contents {
-    jobs <- tmpObject
     jobCount++
+    object := &s3Object{Bucket: bucketName, Key: *tmpObject.Key, Encryption: "????"}
+    DiskLogger.Debug(fmt.Sprintf("getObjectsForBucket(): object=[%v]", object))
+    // wg.Add(1)
+    jobs <- object
   }
-
-  for i := 0; i < jobCount; i++ {
-    objects = append(objects, <-results)
-  }
-  close(results)
 
   if *resp.IsTruncated {
     continuationToken = *resp.NextContinuationToken
-    startAfter = objects[len(objects)-1].Key
+    startAfter = *resp.Contents[jobCount-1].Key
 
-    if (len(objects) % 10000) == 0 {
-      Logger.Info("Found " + strconv.Itoa(len(objects)) + " objects..")
+    getObjectsForBucket(jobs, results, bucketName, continuationToken, startAfter)
+  }
+}
+
+func reportOnBucket(bucketName string) {
+  Logger.Info("Getting list of objects..")
+
+  getObjectsForBucketJobs := make(chan *s3Object)
+  getObjectsForBucketResults := make(chan *s3Object)
+
+  go func() {
+    count := 0
+    for object := range getObjectsForBucketResults {
+      count++
+      if (count % 5000) == 0 {
+        Logger.Info("Found " + strconv.Itoa(count) + " objects..")
+      }
+      DiskLogger.Info(string(object.Key) + " encryption:" + string(object.Encryption))
+      wg.Done()
     }
+  }()
 
-    Logger.Debug("examineObjects(): continuationToken:" + continuationToken + ", startAfter:" + startAfter)
-
-    objects = examineObjects(objects, bucketName, continuationToken, startAfter)
+  for i := 1; i <= workerCount; i++ {
+    go getMetadataWorker(i, getObjectsForBucketJobs, getObjectsForBucketResults)
   }
 
-  return objects
+  wg.Add(1)
+  getObjectsForBucket(getObjectsForBucketJobs, getObjectsForBucketResults, bucketName, "", "")
+  wg.Done()
+
+  wg.Wait()
+  close(getObjectsForBucketResults)
 }
 
-func processBucket(bucketName string, encrypt bool) {
-  objects := objectsFor(bucketName)
+func encryptBucket(bucketName string) {
+  Logger.Info("Getting list of objects..")
 
-  for _, object := range objects {
-    if encrypt == true && object.Encryption != "AES256" {
-      logIt(object.Key + " encryption:" + object.Encryption + " -> AES256")
-      encryptObject(object)
-    } else {
-      logIt(object.Key + " encryption:" + object.Encryption)
+  getObjectsForBucketJobs := make(chan *s3Object)
+  getObjectsForBucketResults := make(chan *s3Object)
+
+  encryptObjectWorkerJobs := make(chan *s3Object)
+  encryptObjectWorkerResults := make(chan *s3Object)
+
+  go func() {
+    count := 0
+    for object := range getObjectsForBucketResults {
+      count++
+      if (count % 5000) == 0 {
+        Logger.Info("Found " + strconv.Itoa(count) + " objects..")
+      }
+      if object.Encryption == neededEncryptionType {
+        DiskLogger.Info(string(object.Key) + " encryption:" + string(object.Encryption) + " updated=false")
+      } else {
+        encryptObjectWorkerJobs <- object
+      }
+      wg.Done()
     }
+  }()
+
+  go func() {
+    count := 0
+    for object := range encryptObjectWorkerResults {
+      count++
+      if (count % 5000) == 0 {
+        Logger.Info("Encrypted " + strconv.Itoa(count) + " objects..")
+      }
+      DiskLogger.Info(string(object.Key) + " encryption:" + string(object.Encryption) + " updated=true")
+      wg.Done()
+    }
+  }()
+
+  for i := 1; i <= workerCount; i++ {
+    go getMetadataWorker(i, getObjectsForBucketJobs, getObjectsForBucketResults)
   }
+
+  for i := 1; i <= workerCount; i++ {
+    go encryptObjectWorker(i, encryptObjectWorkerJobs, encryptObjectWorkerResults)
+  }
+
+  wg.Add(1)
+  getObjectsForBucket(getObjectsForBucketJobs, getObjectsForBucketResults, bucketName, "", "")
+  wg.Done()
+
+  wg.Wait()
+  close(getObjectsForBucketResults)
+  close(encryptObjectWorkerResults)
 }
 
-func encryptObject(object *s3Object) {
-  copySource := fmt.Sprintf("%s/%s", object.Bucket, object.Key)
-
-  params := &s3.CopyObjectInput{
-    Bucket:               aws.String(object.Bucket),
-    Key:                  aws.String(object.Key),
-    CopySource:           aws.String(copySource),
-    ServerSideEncryption: aws.String("AES256"),
+func validateParams(regionName string, bucketName string, roleName string) error {
+  if len(regionName) == 0 {
+    return cli.NewExitError("ERROR: AWS region name is empty", 1)
   }
 
-  _, err := s3Client.CopyObject(params)
-
-  if err != nil {
-    Logger.Error("encryptObject(): s3Client.CopyObject(params) " + err.Error())
-  }
-}
-
-func validateParams(bucketName string, roleName string) error {
   if len(bucketName) == 0 {
     return cli.NewExitError("ERROR: AWS S3 bucket name is empty", 1)
   }
-  if len(roleName) == 0 {
-    return cli.NewExitError("ERROR: AWS IAM role name is empty", 2)
-  }
+
   return nil
 }
 
@@ -246,24 +265,46 @@ func setupLogging(logFileName string, extra string) {
   DiskLogger.Formatter = txtFormatter
   DiskLogger.Out = f
 
+  Logger.Level = logrus.WarnLevel
+  DiskLogger.Level = logrus.InfoLevel
+
   if DEBUG {
     Logger.Level = logrus.DebugLevel
     DiskLogger.Level = logrus.DebugLevel
   } else if VERBOSE {
     Logger.Level = logrus.InfoLevel
-    DiskLogger.Level = logrus.InfoLevel
   }
 }
 
-func main() {
-  var awsRegionName string
-  var credentialsFileName string
-  var bucketName string
-  var roleName string
-  var logFileName string
+func setup(c *cli.Context, style string) (string, error) {
+  logFileName := c.GlobalString("log-file")
+  credentialsFileName := c.GlobalString("credentials")
+  awsRoleName := c.GlobalString("role")
+  awsRegionName := c.GlobalString("region")
+  awsBucketName := c.GlobalString("bucket")
+
+  err := validateParams(awsRegionName, awsBucketName, awsRoleName)
+  if err != nil {
+    return "", err
+  }
+
+  if len(logFileName) == 0 {
+    logFileName = fmt.Sprintf("%s.log", awsBucketName)
+  }
+
+  setupLogging(logFileName, style)
+  setupS3Client(awsRegionName, credentialsFileName, awsRoleName)
 
   numCPUs := runtime.NumCPU()
   runtime.GOMAXPROCS(numCPUs)
+
+  return awsBucketName, nil
+}
+
+func main() {
+  go func() {
+    log.Println(http.ListenAndServe("localhost:6060", nil))
+  }()
 
   app := cli.NewApp()
 
@@ -272,43 +313,6 @@ func main() {
   app.Version = os.Getenv("VERSION")
 
   app.Flags = []cli.Flag{
-    cli.StringFlag{
-      Name:        "aws-region",
-      Usage:       "AWS region",
-      EnvVar:      "AWS_REGION",
-      Destination: &awsRegionName,
-    },
-    cli.StringFlag{
-      Name:        "credentials, c",
-      Usage:       "AWS credentials location",
-      EnvVar:      "CREDENTIALS_FILE_NAME",
-      Destination: &credentialsFileName,
-    },
-    cli.StringFlag{
-      Name:        "bucket, b",
-      Usage:       "AWS S3 bucket",
-      EnvVar:      "BUCKET_NAME",
-      Destination: &bucketName,
-    },
-    cli.StringFlag{
-      Name:        "role, r",
-      Usage:       "AWS IAM role",
-      EnvVar:      "ROLE_NAME",
-      Destination: &roleName,
-    },
-    cli.StringFlag{
-      Name:        "log-file-name, l",
-      Usage:       "Log file name",
-      EnvVar:      "LOG_FILE_NAME",
-      Destination: &logFileName,
-    },
-    cli.IntFlag{
-      Name:        "workers, 1",
-      Usage:       "Worker count (default is number of CPU's x 16)",
-      EnvVar:      "WORKER_COUNT",
-      Value:       runtime.NumCPU() * 16,
-      Destination: &workerCount,
-    },
     cli.BoolFlag{
       Name:        "verbose",
       Usage:       "Verbose mode",
@@ -321,6 +325,38 @@ func main() {
       EnvVar:      "DEBUG",
       Destination: &DEBUG,
     },
+    cli.IntFlag{
+      Name:        "workers",
+      Usage:       "Worker count",
+      EnvVar:      "WORKER_COUNT",
+      Value:       runtime.NumCPU() * 8,
+      Destination: &workerCount,
+    },
+    cli.StringFlag{
+      Name:   "log-file, l",
+      Usage:  "Log file name",
+      EnvVar: "LOG_FILE",
+    },
+    cli.StringFlag{
+      Name:   "credentials, c",
+      Usage:  "AWS credentials location",
+      EnvVar: "CREDENTIALS",
+    },
+    cli.StringFlag{
+      Name:   "role",
+      Usage:  "AWS IAM role",
+      EnvVar: "AWS_ROLE",
+    },
+    cli.StringFlag{
+      Name:   "region",
+      Usage:  "AWS region",
+      EnvVar: "AWS_REGION",
+    },
+    cli.StringFlag{
+      Name:   "bucket",
+      Usage:  "AWS S3 bucket",
+      EnvVar: "AWS_BUCKET",
+    },
   }
 
   app.Commands = []cli.Command{
@@ -329,18 +365,12 @@ func main() {
       Usage:       "Report",
       Description: "Provide a report of the encryption status of each object",
       Action: func(c *cli.Context) error {
-        err := validateParams(bucketName, roleName)
+        awsBucketName, err := setup(c, "report")
         if err != nil {
           return err
         }
 
-        if len(logFileName) == 0 {
-          logFileName = fmt.Sprintf("%s.log", bucketName)
-        }
-
-        setupLogging(logFileName, "report")
-        s3Client = getS3Client(awsRegionName, credentialsFileName, roleName)
-        processBucket(bucketName, false)
+        reportOnBucket(awsBucketName)
         return nil
       },
     },
@@ -349,18 +379,12 @@ func main() {
       Usage:       "Encrypt",
       Description: "Encrypt every object in the bucket",
       Action: func(c *cli.Context) error {
-        err := validateParams(bucketName, roleName)
+        awsBucketName, err := setup(c, "encrypt")
         if err != nil {
           return err
         }
 
-        if len(logFileName) == 0 {
-          logFileName = fmt.Sprintf("%s.log", bucketName)
-        }
-
-        setupLogging(logFileName, "encrypt")
-        s3Client = getS3Client(awsRegionName, credentialsFileName, roleName)
-        processBucket(bucketName, true)
+        encryptBucket(awsBucketName)
         return nil
       },
     },
